a3d4b06df8ec0a01b8b966187a84d2d2117367d4
coder1408 <ishanyateenjoshi@gmail.com>
2024-03-04 20:20:20 +0530

    change

--- BabaStocksScanners/Screeners/emails.py
+++ BabaStocksScanners/Screeners/emails.py
@@ -1,16 +1,18 @@
 import os
-import smtplib, ssl
+import smtplib
+import ssl  # Add this line for ssl module
 from email.message import EmailMessage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 from dotenv import load_dotenv
 from datetime import date
-import scanner
 
 PORT = 587
 EMAIL_SERVER = 'smtp-mail.outlook.com'
 today = date.today()
-d1 = today.strftime("%d.%m.%Y")
+d1 = today.strftime("%d-%m-%Y")
 
 envars = r'StockProject\BabaStocksScanners\.env'
 load_dotenv(envars)
@@ -18,7 +20,7 @@ load_dotenv(envars)
 sender_email = os.getenv("EMAIL")
 password_email = os.getenv("PASSWORD")
 
-def send_email(subject, receiver_emails, name):
+def send_email(subject, receiver_emails, name, attachment_path):
     msg = MIMEMultipart()
     msg['Subject'] = subject
     msg['From'] = f'The Yoga of TA <{sender_email}>'
@@ -26,32 +28,20 @@ def send_email(subject, receiver_emails, name):
 
     text_content = MIMEText(f"""\
         Hi {name},
-        Please find your stock recommendations from "THE YOGA OF TA" for the date "{d1}".
+        Please find your stock screener requirements from "THE YOGA OF TA" for the date "{d1}".
         
-        Volume breakout along with moving average crossover and bounce For Cash:
-        {scanner.stock_list_cash_screener_string}
-
-        Volume breakout along with moving average crossover and bounce For Nifty 50:
-        {scanner.stock_list_nifty_50_screener_string}
-
-        Volume breakout along with moving average crossover and bounce For Nifty 100:
-        {scanner.stock_list_nifty_100_screener_string}
-
-        Volume breakout along with moving average crossover and bounce For Nifty 200:
-        {scanner.stock_list_nifty_200_screener_string}
-
-        Volume breakout along with moving average crossover and bounce For Nifty 500:
-        {scanner.stock_list_nifty_500_screener_string}
-        
-        Volume breakout along with moving average crossover and bounce For Midcap 50:
-        {scanner.stock_list_midcap_50_screener_string}
-        
-
         Best regards,
         THE YOGA OF TA TEAM
     """)
-
     msg.attach(text_content)  # Attach the text content to the email message
+
+    # Attach the file
+    attachment = open(attachment_path, "rb")
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(attachment.read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', f'attachment; filename="{os.path.basename(attachment_path)}"')
+    msg.attach(part)
 
     # Create a secure SSL context
     context = ssl.create_default_context()
@@ -67,6 +57,7 @@ if __name__ == "__main__":
     send_email(
         subject="Today's Stock Recommendations",
         name="Yateen",
-        receiver_emails=['yateenjoshi@gmail.com', 'ishanprivate14@gmail.com', 'suruchigtm@gmail.com', 'prafulla.huddar@gmail.com', 'divatema@gmail.com', 'aru.3412@gmail.com'],
+        receiver_emails=['yateenjoshi@gmail.com', 'ishanprivate14@gmail.com'],
+        attachment_path=rf"C:\Users\ishan\Downloads\PortfolioAnalysisPaid\PortfolioAnalysis{d1}.xlsx",  # Replace with the actual path to your file
     )
     print("After")
